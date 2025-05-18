ebb28817fed1d0d42e03e9c56a1f2fbd2a7ddb17
Neeraj Kumar <neerajkumarkondabattula@gmail.com>
2025-05-18 23:21:06 +0530

    topbar updated

--- client/src/components/Layout/Topbar.jsx
+++ client/src/components/Layout/Topbar.jsx
@@ -2,31 +2,32 @@ import React from "react";
 import { TbBrandMeta } from "react-icons/tb";
 import { RiTwitterXLine } from "react-icons/ri";
 import { IoLogoInstagram } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Topbar = () => {
   return (
     <div className="bg-red-700 text-white font-Poppins">
       <div className="container mx-auto flex justify-between items-center py-3 px-8">
         <div className="hidden md:flex items-center space-x-4">
-          <a href="#" className="hover:text-gray-300">
+          <Link to="#" className="hover:text-gray-300">
             <TbBrandMeta className="h-5 w-5" />
-          </a>
-          <a href="#" className="hover:text-gray-300">
+          </Link>
+          <Link to="#" className="hover:text-gray-300">
             <IoLogoInstagram className="h-5 w-5" />
-          </a>
-          <a href="#" className="hover:text-gray-300">
+          </Link>
+          <Link to="#" className="hover:text-gray-300">
             <RiTwitterXLine className="h-4 w-5" />
-          </a>
+          </Link>
         </div>
         <div className="text-sm text-center flex-grow">
-          <a href="#" className="hover:text-gray-300">
+          <Link to="#" className="hover:text-gray-300">
             <span>We ship world wide - fast and reliable.</span>
-          </a>
+          </Link>
         </div>
         <div className="hidden md:block text-sm">
-          <a href="tel:8019755437" className="hover:text-gray-300">
+          <Link to="tel:8019755437" className="hover:text-gray-300">
             +91 8019755437
-          </a>
+          </Link>
         </div>
       </div>
     </div>
