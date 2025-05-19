8cc6231f1d7f70abe5023718df97d1b92d5c9c6f
Neeraj Kumar <neerajkumarkondabattula@gmail.com>
2025-05-19 20:42:37 +0530

    Navbar Added

--- client/src/components/Common/Header.jsx
+++ client/src/components/Common/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Topbar from "../Layout/Topbar";
+import Navbar from "../Layout/Navbar";
 
 const Header = () => {
   return (
     <div>
       <Topbar />
+      <Navbar />
     </div>
   );
 };
---
+++ client/src/components/Layout/Navbar.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  HiBars3BottomRight,
+  HiOutlineShoppingBag,
+  HiOutlineUser,
+} from "react-icons/hi2";
+
+const Navbar = () => {
+  return (
+    <div className="bg-[#121239] text-white">
+      <nav className="relative container mx-auto flex items-center justify-between py-4 px-8 font-Poppins">
+        <div className="z-10">
+          <Link to="/" className="font-medium text-2xl">
+            Reginald Men
+          </Link>
+        </div>
+
+        <div className="absolute left-1/2 transform -translate-x-1/2 hidden md:flex space-x-6 uppercase">
+          <Link
+            to="/"
+            className="group relative text-white text-sm font-medium"
+          >
+            <span className="border-0 pb-1">Home</span>
+            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-white transition-all duration-300 ease-in-out group-hover:w-full"></span>
+          </Link>
+          <Link
+            to="/shop"
+            className="group relative text-white text-sm font-medium"
+          >
+            <span className="border-0 pb-1">Shop</span>
+            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-white transition-all duration-300 ease-in-out group-hover:w-full"></span>
+          </Link>
+          <Link
+            to="/track"
+            className="group relative text-white text-sm font-medium"
+          >
+            <span className="border-0 pb-1">Track</span>
+            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-white transition-all duration-300 ease-in-out group-hover:w-full"></span>
+          </Link>
+
+          <Link
+            to="/contact"
+            className="group relative text-white text-sm font-medium"
+          >
+            <span className="border-0 pb-1">Contact</span>
+            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-white transition-all duration-300 ease-in-out group-hover:w-full"></span>
+          </Link>
+          <Link
+            to="/about"
+            className="group relative text-white text-sm font-medium"
+          >
+            <span className="border-0 pb-1">About</span>
+            <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-white transition-all duration-300 ease-in-out group-hover:w-full"></span>
+          </Link>
+        </div>
+
+        <div className="flex items-center space-x-2 z-10">
+          <Link to="/profile" className="hover:text-black">
+            <HiOutlineUser className="h-6 w-6 text-white" />
+          </Link>
+          <button className="relative hover:text-black">
+            <HiOutlineShoppingBag className="h-5 w-6 text-white" />
+            <span className="absolute -top-1 bg-white text-[#121239] rounded-full text-xs px-1 font-medium">
+              5
+            </span>
+          </button>
+          <button className="md:hidden hover:text-black">
+            <HiBars3BottomRight className="h-6 w-6 text-white" />
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
