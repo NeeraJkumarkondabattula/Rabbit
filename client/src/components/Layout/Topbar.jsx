77a51064577784079ec0f93b62fea709d12dc5b6
Neeraj Kumar <neerajkumarkondabattula@gmail.com>
2025-05-19 13:24:08 +0530

    topbar updated

--- client/src/components/Layout/Topbar.jsx
+++ client/src/components/Layout/Topbar.jsx
@@ -1,28 +1,76 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { TbBrandMeta } from "react-icons/tb";
 import { RiTwitterXLine } from "react-icons/ri";
 import { IoLogoInstagram } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 const Topbar = () => {
+  const items = [
+    {
+      text: "We ship world wide - fast and reliable.",
+      link: "/shipping",
+    },
+    {
+      text: "Get your loved products with good offers!",
+      link: "/offers",
+    },
+    {
+      text: "Summer sale is ending soon.",
+      link: "/summer-sale",
+    },
+  ];
+
+  const [index, setIndex] = useState(0);
+  const [direction, setDirection] = useState("in"); // "in" or "out"
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDirection("out");
+    }, 3500); // Start fade-out before full 4 seconds
+
+    const interval = setInterval(() => {
+      setDirection("in");
+      setIndex((prev) => (prev + 1) % items.length);
+    }, 4000);
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timer);
+    };
+  }, [index]);
+
   return (
     <div className="bg-red-700 text-white font-Poppins">
       <div className="container mx-auto flex justify-between items-center py-3 px-8">
         <div className="hidden md:flex items-center space-x-4">
-          <Link to="#" className="hover:text-gray-300">
+          <Link to="https://www.facebook.com/" className="hover:text-gray-300">
             <TbBrandMeta className="h-5 w-5" />
           </Link>
-          <Link to="#" className="hover:text-gray-300">
+          <Link to="https://www.instagram.com/" className="hover:text-gray-300">
             <IoLogoInstagram className="h-5 w-5" />
           </Link>
-          <Link to="#" className="hover:text-gray-300">
+          <Link to="https://x.com/login" className="hover:text-gray-300">
             <RiTwitterXLine className="h-4 w-5" />
           </Link>
         </div>
-        <div className="text-sm text-center flex-grow">
-          <Link to="#" className="hover:text-gray-300">
-            <span>We ship world wide - fast and reliable.</span>
-          </Link>
+        <div className="relative overflow-hidden h-6 text-sm text-center flex-grow">
+          <div className="absolute inset-0 flex items-center justify-center w-full">
+            <Link
+              to={items[index].link}
+              className={`
+            transition-all duration-700 ease-in-out
+            inline-block whitespace-nowrap
+            ${
+              direction === "in"
+                ? "translate-x-0 opacity-100"
+                : "-translate-x-8 opacity-0"
+            }
+          `}
+              onAnimationEnd={() => setDirection("in")}
+            >
+              <span>{items[index].text}</span>
+            </Link>
+          </div>
         </div>
         <div className="hidden md:block text-sm">
           <Link to="tel:8019755437" className="hover:text-gray-300">
