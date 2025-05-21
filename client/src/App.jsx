a6a0511088afa176ba57abded19d34f24eb007b4
Neeraj Kumar <neerajkumarkondabattula@gmail.com>
2025-05-21 19:47:35 +0530

    Footer Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Topbar from "./components/Layout/Topbar";
 import Header from "./components/Common/Header";
+import Footer from "./components/Layout/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         </Route>
         <Route>{/* Admin Layout */}</Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ client/src/components/Layout/Footer.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { TbBrandMeta } from "react-icons/tb";
+import { RiTwitterXLine } from "react-icons/ri";
+import { IoLogoInstagram } from "react-icons/io";
+import { MdOutlineMail } from "react-icons/md";
+import { IoCallOutline } from "react-icons/io5";
+
+const Footer = () => {
+  return (
+    <footer className="border-t py-12 bg-[#121212] text-white">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-4 px-8">
+        <div>
+          <h3 className="text-lg text-whi mb-4">Newsletter</h3>
+          <p className="text-gray-500 mb-4 text-sm">
+            Be the first to hear about new products, exclusive events, and
+            online offers.
+          </p>
+          <p className="text-sm font-medium">
+            Sign up and get 10% off your first order.
+          </p>
+          <form className="flex mt-2">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-full text-sm border-t border-l border-b rounded-l-md focus:outline-none focus:ring-2 focus:ring-gray-800 transition-all required px-3 py-2"
+            />
+            <button className="bg-black text-white text-sm rounded-r-md hover:bg-gray-800 px-4 py-2">
+              Subscribe
+            </button>
+          </form>
+        </div>
+        <div>
+          <h3 className="text-lg text-white mb-4">Shop</h3>
+          <ul className="space-y-2 text-gray-500 text-sm">
+            <li>
+              <Link to="/" className="hover:text-white transition-colors">
+                Sunscreen
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="hover:text-white transition-colors">
+                Serum
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="hover:text-white transition-colors">
+                Facewash
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="hover:text-white transition-colors">
+                Lipbalm
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="hover:text-white transition-colors">
+                Moisturizer
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-lg text-white mb-4">Support</h3>
+          <ul className="space-y-2 text-gray-500 text-sm">
+            <li>
+              <Link to="/" className="hover:text-white transition-colors">
+                Product Disclaimer
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="hover:text-white transition-colors">
+                Contact Us
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="hover:text-white transition-colors">
+                Terms of Service
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="hover:text-white transition-colors">
+                Retun & Shipping Policy
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="hover:text-white transition-colors">
+                Privacy Policy
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-lg text-white mb-2">Follow Us</h3>
+          <ul className="space-x-2 text-gray-500 flex text-sm">
+            <li>
+              <Link
+                to="https://www.facebook.com/"
+                className="hover:text-gray-300"
+              >
+                <TbBrandMeta className="h-5 w-5" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="https://www.instagram.com/"
+                className="hover:text-gray-300"
+              >
+                <IoLogoInstagram className="h-5 w-5" />
+              </Link>
+            </li>
+            <li>
+              <Link to="https://x.com/login" className="hover:text-gray-300">
+                <RiTwitterXLine className="h-4 w-5" />
+              </Link>
+            </li>
+          </ul>
+          <div className=" items-center text-gray-500 mt-8">
+            <h3 className="text-lg text-white mb-2">Contact Us</h3>
+            <Link to="mailt0:info@reginaldmen.com" className="block">
+              <MdOutlineMail className="w-6 h-5 inline-block text-sm" />{" "}
+              info@reginaldmen.com
+            </Link>
+            <Link to="tel:+918019755437" className="block">
+              <IoCallOutline className="w-6 h-5 inline-block text-sm" /> +91
+              8019755437
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
